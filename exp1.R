cacf8911ca306f9b92fdfa011467e652a1780502
Peter Tempfli <tempflip@gmail.com>
2019-09-25 17:30:55 +0200

    w

--- exp1.R
+++ exp1.R
@@ -1,11 +1,15 @@
 df <- read.csv("~/Desktop/uni/thesis_repo/KVH_data.csv")
+months = names(df)[7:18]
+df['row_mean'] = apply(df[,months],1,mean)
+df$row_mean = sqrt(df$row_mean)
 library(ggplot2)
 
 # on map
 ggplot(data=df, aes(x=Lon, y=Lat)) + geom_point()
 
 # monthly consumpltion plot
-months = names(df)[7:18]
+
+
 m = df[,months]
 m['id'] = seq(1:220)
 m_long <- melt(m, id.vars=c('id'))
@@ -21,7 +25,10 @@ ggplot(data=m_norm_by_mean_long, aes(x=variable, y=value, group=id)) + geom_line
 
 # mapping extra
 library(leaflet)
+
+pal <- colorNumeric(palette = "inferno", domain = df$row_mean)
+
 mp <- leaflet(data = df) %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
-  addCircleMarkers(~Lon, ~Lat, radius = ~jan)
+  addCircleMarkers(~Lon, ~Lat, radius = ~row_mean/5, color=~pal(row_mean))
 mp
